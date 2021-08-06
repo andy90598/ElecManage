b7341b7c28e9a6428dadfba13e935c5bb62dd3ed
andy90598 <1105137110@nkust.edu.tw>
2021-08-06 13:44:08 +0800

    fix

--- ElecManage/Worker.cs
+++ ElecManage/Worker.cs
@@ -52,12 +52,16 @@ namespace ElecManage
                     var datas = _httpClient.OnGet().Result;
                     //把取道的資料塞到資料庫
                     await AddDataAsync(dbCtx, datas);
-                    await Calc(dbCtx);
+                    CalcAsync(dbCtx);
+                    CalcHourAsync(dbCtx);
+                    CalcDateAsync(dbCtx);
+                    CalcMonthAsync(dbCtx);
                     await Task.Delay(10000, stoppingToken);
                     //
                 }
 
             }
+            //存資料到資料庫
             async Task AddDataAsync(ElecDBContext dbCtx,List<ElecData>datas)
             {
                 foreach (ElecData x in datas)
@@ -75,7 +79,8 @@ namespace ElecManage
                 
                 await _hub.Clients.All.SendAsync("transferdata", datas);
             }
-            async Task Calc(ElecDBContext dbCtx)
+            
+            async Task CalcAsync(ElecDBContext dbCtx)
             {
                 var valueList = dbCtx.Electricities
                     .Select(g => new
@@ -88,23 +93,77 @@ namespace ElecManage
                     .OrderByDescending(x=>x.sn).Take(2)
                     .ToList();
                 await _hub.Clients.All.SendAsync("transferdataBar", valueList);
-                //計算時間內有幾筆
-                //var count = dbCtx.Electricities
-                //    .Where(x => x.Time.Date == DateTime.Now.Date && x.Time.Hour == DateTime.Now.Hour && x.DeviceId == "0081F924C0C6").Count();
-
-                //var valueList = dbCtx.Electricities
-                //    .Where(x => x.Time.Date == DateTime.Now.Date && x.Time.Hour == DateTime.Now.Hour && x.Time.Minute == DateTime.Now.Minute)
-                //    .GroupBy(x => x.DeviceId)
-                //    .Select(g => new
-                //    {
-                //        uuid = g.Key,
-                //        //平均電流*110(電壓)/60
-                //        SUM = Math.Round((decimal)g.Sum(s => s.Value)/count,2),
-
-                //        Time = g.Max(s => s.Time).GetDateTimeFormats('g')[0]
-                //    }).ToList();
-                //await _hub.Clients.All.SendAsync("transferdataBar", valueList);
             }
+            //每天每小時的用量
+            async Task CalcHourAsync(ElecDBContext dbCtx)
+            {
+                //抓前一個小時完整資料
+                //var comparedate = DateTime.Now.AddHours(-1);
+                var hourList = dbCtx.Electricities
+                        //.Where(x => x.Time.Date == comparedate.Date && x.Time.Hour == comparedate.Hour)
+                        //x.Time.Date == DateTime.Now.Date && x.Time.Hour== DateTime.Now.Hour - 1
+                        //用日期 
+                        .GroupBy(x=>new 
+                        {
+                            Date =x.Time.Date,
+                            Hour =x.Time.Hour,
+                            ID =x.DeviceId
+                        })
+                        .Select(g => new
+                        {
+                            uuid = g.Key.ID,
+                            //1小時平均電流
+                            SUM = Math.Round((decimal)g.Average(s => s.Value), 2),
+                            time=DateTime.Parse(g.Key.Date.GetDateTimeFormats('d')[0]+' '+g.Key.Hour.ToString()+ ":00:00"),
+                        }).ToList();
+                //signalR 推送資料
+                await _hub.Clients.All.SendAsync("transferdataHour", hourList);    
+            }
+            //每天用量
+            async Task CalcDateAsync(ElecDBContext dbCtx)
+            {
+                var dateList = dbCtx.Electricities
+                        //.Where(x => x.Time.Date == comparedate.Date && x.Time.Hour == comparedate.Hour)
+                        //x.Time.Date == DateTime.Now.Date && x.Time.Hour== DateTime.Now.Hour - 1
+                        //用日期 
+                        .GroupBy(x => new
+                        {
+                            Date = x.Time.Date,
+                            ID = x.DeviceId
+                        })
+                        .Select(g => new
+                        {
+                            uuid = g.Key.ID,
+                            SUM = Math.Round((decimal)g.Average(s => s.Value), 2),
+                            time = g.Key.Date.GetDateTimeFormats('d')[0],
+                        }).ToList();
+                //signalR 推送資料
+                await _hub.Clients.All.SendAsync("transferdataDate", dateList);
+            }
+            //每月用量
+            async Task CalcMonthAsync(ElecDBContext dbCtx)
+            {
+                var dateList = dbCtx.Electricities              
+                        //x.Time.Date == DateTime.Now.Date && x.Time.Hour== DateTime.Now.Hour - 1
+                        //用日期 
+                        .GroupBy(x => new
+                        {
+                            Date = x.Time.Date.Month,
+                            ID = x.DeviceId
+                        })
+                        .Select(g => new
+                        {
+                            uuid = g.Key.ID, 
+                            SUM = Math.Round((decimal)g.Average(s => s.Value), 2),
+                            time = g.Key.Date
+                        })                                         
+                        .ToList()
+                        .OrderBy(z => z.uuid);
+                
+                //signalR 推送資料               
+                await _hub.Clients.All.SendAsync("transferdataMonth", dateList);
+            }
+
         }
     }
 }
