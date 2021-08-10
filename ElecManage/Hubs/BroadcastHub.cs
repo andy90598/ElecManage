51e91c07f1fe8adb68fdefcb275daaeba9e041b0
andy90598 <1105137110@nkust.edu.tw>
2021-08-10 10:21:32 +0800

    大改前

--- ElecManage/Hubs/BroadcastHub.cs
+++ ElecManage/Hubs/BroadcastHub.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using ElecManage.Models;
+using ElecManage.Services;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,8 +8,20 @@ using System.Threading.Tasks;
 
 namespace ElecManage.Hubs
 {
-    public class BroadcastHub :Hub<IHubClient>
+    public class BroadcastHub :Hub
     {
-        
+        private readonly ElecDBContext _context;
+        private readonly IHubContext<BroadcastHub> _hubContext;
+        private readonly IHttpClient _httpClient;
+        public BroadcastHub(ElecDBContext context, IHttpClient httpClient)
+        {
+            _context = context;
+            _httpClient = httpClient;
+        }
+        public Task ReturnDashBoardDataToAll()
+        {
+            var data = _httpClient.OnGet().Result;
+            return Clients.All.SendAsync("RefreshDashBoardData", data);
+        }
     }
 }
